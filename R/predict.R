99a7cbd7d98cb5ea877a95fce65a60d9f9a125f8
hoxo-m <hoxo.smile@gmail.com>
2016-12-07 23:12:09 +0900

    fix

---
+++ R/predict.R
@@ -0,0 +1,7 @@
+#' @export
+predict.cpls <- function(object, newdata, ncomp = 1:object$ncomp, comps,
+                         type = c("response", "scores"), na.action = na.pass, ...) {
+  x <- model.frame(object$formula, data = newdata)[, -1, drop=FALSE]
+  pred <- t(object$alpha) %*% t(object$w) %*% t(x) + object$center
+  as.vector(ifelse(pred >= 0.5, 1, 0))
+}
