1cdb0e09a11fa8239468c9a9c5fe23002c866cc6
hoxo-m <hoxo.smile@gmail.com>
2016-12-21 22:34:03 +0900

    fix

--- tests/testthat/test-gcm-prod.R
+++ tests/testthat/test-gcm-prod.R
@@ -4,13 +4,6 @@ test_that("prod for GCM", {
   x <- matrix(sample(0:1, size = 100, replace = TRUE), nrow=10)
   gcm <- GrammarCompressedMatrix$new(x, re_pair_method ="plain", verbose = FALSE)
   mat <- matrix(1:10)
-  # print(x[c(2,7,9,10),])
-  # print(gcm$access_row(2))
-  # print(gcm$access_row(7))
-  # print(gcm$access_row(9))
-  # print(gcm$access_row(10))
-  # print(gcm$.__enclos_env__$private$.compressed_matrix[c(2,7,9,10)])
-  # print(gcm$.__enclos_env__$private$.grammer_trees)
   expect_identical(gcm_prod(gcm, mat), x %*% mat)
 })
 
